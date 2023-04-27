c14a5acb37768ec6106e34844055f74b37a814de
atsupi <atsupi@gmail.com>
2023-04-28 00:30:32 +0900

    Initial commit.

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("node.js started");
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const listener = app.listen(PORT, () => {
+    console.log("port: %d", listener.address().port);
+});
+
+app.use(express.static('src'));
+app.get("/test", (req, res) => {
+    res.contentType('html');
+    res.status(200).sendFile(__dirname + "/test/index.html");
+    console.log("GET: test/index.html");
+});
+
+app.set('view engine', 'hbs');
+app.get('', (req, res) => {
+    res.render('index', {
+        title: "Jade Seeker",
+        description: "Jane is seeking for Jade."
+    });
+})
